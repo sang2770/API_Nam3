6362ddd75efbb07fad16bbfd55fdcc922e353ac6
hung <hung71718@gmail.com>
2022-04-17 14:42:19 +0700

    create move

---
+++ BTL_APIMOVIE/BTL_APIMOVIE/Controllers/CountrysController.cs
@@ -0,0 +1,110 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BTL_APIMOVIE.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BTL_APIMOVIE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountrysController : ControllerBase
+    {
+        // GET: api/<CountrysController>
+        private readonly APIMOVIESContext _context;
+
+        public CountrysController(APIMOVIESContext context)
+        {
+            _context = new APIMOVIESContext();
+        }
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbQuocgia>>> GetTbQuocGia()
+        {
+            return await _context.TbQuocgia.ToListAsync();
+        }
+
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbQuocgia>> GetTbQuocGia(int id)
+        {
+            var tbQuocgia = await _context.TbQuocgia.FindAsync(id);
+
+            if (tbQuocgia == null)
+            {
+                return NotFound();
+            }
+
+            return tbQuocgia;
+        }
+
+        // PUT: api/Category/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbQuocgia(int id, TbQuocgia tbQuocgia)
+        {
+            if (id != tbQuocgia.Maquocgia)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbQuocgia).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbQuocgiaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Category
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbQuocgia>> PostTbLoaiphim(TbQuocgia tbQuocgia)
+        {
+            _context.TbQuocgia.Add(tbQuocgia);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTbQuocGia", new { id = tbQuocgia.Maquocgia }, tbQuocgia);
+        }
+
+        // DELETE: api/Category/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGetTbQuocGia(int id)
+        {
+            var tbQuocgia = await _context.TbQuocgia.FindAsync(id);
+            if (tbQuocgia== null)
+            {
+                return NotFound();
+            }
+
+            _context.TbQuocgia.Remove(tbQuocgia);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbQuocgiaExists(int id)
+        {
+            return _context.TbQuocgia.Any(e => e.Maquocgia == id);
+        }
+    }
+}
